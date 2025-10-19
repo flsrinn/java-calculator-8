b449e8f707240cee758c87d87be8983fb8305cbb
Ahrin Jun <wjsdkfls03@naver.com>
2025-10-20 00:17:26 +0900

    chore(test): 불필요한 import 문 제거

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,10 +1,7 @@
 package calculator;
 
-import calculator.service.TokenSplitter;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
