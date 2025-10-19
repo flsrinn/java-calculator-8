f357ba72883c8cc41d31721a6949cb27365aee04
Ahrin Jun <wjsdkfls03@naver.com>
2025-10-19 21:27:07 +0900

    test(app): 예외 처리 검증용 테스트 코드 추가
    - 연속 구분자, 마지막 구분자, 비숫자 포함, 0 포함, 커스텀 형식 오류 테스트 추가

---
+++ src/test/java/calculator/ExceptionTest.java
@@ -0,0 +1,54 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ExceptionTest extends NsTest {
+    @Test
+    void 연속_구분자_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,,2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 마지막_구분자_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2,"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 비숫자_포함_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,a,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 제로_포함_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("0,1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_형식_오류_예외_줄바꿈_누락() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;1;2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
